4a25becf4b1709053157ca68f0f3d5b477ddd7dd
Riju <mailmeonriju@gmail.com>
2017-05-11 08:17:05 +0530

    mail2

--- kumar_dental_clinic/server_scripts/sendMail.php
+++ kumar_dental_clinic/server_scripts/sendMail.php
@@ -6,18 +6,26 @@ switch ($type) {
 	case 'appt':
 		sendApptMail();
 		break;
+	case 'contact':
+		sendContactMail();
+		break;
 	
 	default:
 		returnJSON();
 		break;
 }
 
-sendContactMail();
+// $to = "riteshkumar26061990@gmail.com,admin@kumardentalclinic.in";
+$to = "mailmeonriju@gmail.com";
+
+
+$headers = "";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+// $headers .= 'Bcc: mailmeonriju@gmail.com,sumitas.designer@gmail.com' . "\r\n";
+ 	
 
 function sendApptMail(){
-	$to = "mailmeonriju@gmail.com";
-	// $from = $_POST["from"];
-	// $type = $_POST["type"];
+
 	$subject = "Make an appointment";
 	$name = $_POST["name"];
 	$address = $_POST["address"];
@@ -29,14 +37,32 @@ function sendApptMail(){
 	$message .= 'phone : '.$phone.'<br>';
 	$message .= 'timing : '.$timing.'<br>';
 
- 	$status = mail($to,$subject,$message);
+}
+
+function sendContactMail(){
+
+	$subject = "Contact Us";
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$telephone = $_POST["telephone"];
+	$message_txt = $_POST["message"];
+
+	$message = 'name : '.$name.'<br>';
+	$message .= 'email : '.$email.'<br>';
+	$message .= 'telephone : '.$telephone.'<br>';
+	$message .= 'message : '.$message_txt.'<br>';
+
+}
+
+function returnJSON($arr=array('status'=>'fail','message'=>'unknown error')){
+	echo json_encode($arr);exit;
+}
+
+function sendMail(){
+	$status = mail($to,$subject,$message,$headers);
 	if ($status) {
 		returnJSON($arr=array('status'=>'success','message'=>'mail sent'));
 	} else {
 		returnJSON();
 	}
-}
-
-function returnJSON($arr=array('status'=>'fail','message'=>'unknown error')){
-	echo json_encode($arr);exit;
 }
